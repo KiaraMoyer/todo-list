5df469c58678a2e8b205910362ec5dfedc66ab50
Kiara Moyer <kiara.moyer.11@gmail.com>
2019-10-02 13:57:58 -0500

    Refactor: switch to JSX

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import TaskTable from './ToDoList.js';
 
 function App() {
   return (
-  React.createElement('div', {className: 'App'},
-    React.createElement(TaskTable)
-    )
+    <div className="App">
+      <TaskTable />
+    </div>
   )
 }
 
